ea6e3bafca8eb57e105b1394695cac2d726a9f36
waghvishalg <waghvishalg@gmail.com>
2024-01-04 17:03:13 +0530

    Project Setup commit

---
+++ ToDoListApp/ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Vishal Wagh on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
