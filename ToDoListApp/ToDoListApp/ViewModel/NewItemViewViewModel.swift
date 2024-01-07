dcde30fdfae145c5a293798d0558aa27e559590a
waghvishalg <waghvishalg@gmail.com>
2024-01-07 11:29:03 +0530

    Create the new item and save to firebase data base with all functionality

--- ToDoListApp/ToDoListApp/ViewModel/NewItemViewViewModel.swift
+++ ToDoListApp/ToDoListApp/ViewModel/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Vishal Wagh on 04/01/24.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -15,7 +17,29 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
+        //Get current user id
+        guard let uId = Auth.auth().currentUser?.uid else { return }
+        
+        // create Model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createDate: Date().timeIntervalSince1970,
+            isDone: false)
+        
+        // Save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
